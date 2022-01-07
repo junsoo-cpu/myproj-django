c220b438784fe2bd17af3005557cfa64946271de
JunSoo Lee <ljsay1113@naver.com>
2022-01-07 16:53:05 +0900

    리뷰 작성 모델 구현

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from shop.views import ReviewViewSet
-
+from shop.views import ReviewViewSet, review_list, review_new
 
 app_name = "shop"
 
@@ -9,5 +8,7 @@ router = DefaultRouter()
 router.register("reviews", ReviewViewSet)
 
 urlpatterns = [
+    path("reviews/", review_list, name="review_list"),
+    path("review/new/", review_new, name="review_new"),
     path("api/", include(router.urls)),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,19 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from rest_framework import viewsets
+
+from shop.form import ReviewForm
 from shop.models import Review
 from shop.serializers import ReviewSerializer
+
+review_list = ListView.as_view(model=Review)
+
+review_new = CreateView.as_view(
+    model=Review,
+    form_class=ReviewForm,
+    success_url=reverse_lazy("shop:review_list"),
+
+)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
