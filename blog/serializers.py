145f0635b8cc5e1cc28e8adcd85b06cca16f52ee
JunSoo Lee <ljsay1113@naver.com>
2022-01-10 15:14:12 +0900

    수정 작업

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,4 +5,4 @@ from blog.models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = " __all__" # 실 서비스에는 비추
+        fields = "__all__" # 실 서비스에는 비추
