299dad3e29d342ffc2abd58cd25ed124e2260d51
JunSoo Lee <ljsay1113@naver.com>
2022-01-13 10:42:42 +0900

    경로 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from blog.views import PostViewSet
@@ -12,6 +10,3 @@ router.register("posts", PostViewSet)
 urlpatterns = [
     path("api/", include(router.urls)),
 ]
-
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
