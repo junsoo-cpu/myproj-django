926e1d994e12d3ab2ef184d41e4aef01bb480988
JunSoo Lee <ljsay1113@naver.com>
2022-01-18 10:17:25 +0900

    access / refresh 속성 외 추가

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,13 +7,22 @@ from rest_framework_simplejwt.serializers import (
 
 
 class TokenObtainPairSerializer(OrigTokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user) -> Dict:
-        token = super().get_token(user)
-        token['username'] = user.username
-        token['first_name'] = user.first_name
-        token['last_name'] = user.last_name
-        return token
+
+    # JMT playload 커스텀
+    # @classmethod
+    # def get_token(cls, user) -> Dict:
+    #     token = super().get_token(user)
+    #     token['username'] = user.username
+    #     token['first_name'] = user.first_name
+    #     token['last_name'] = user.last_name
+    #     return token
+
+    # access/refresh 속성 외 추가
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['first_name'] = self.user.first_name
+        data['last_name'] = self.user.last_name
 
 
 class TokenRefreshSerializer(OrigTokenRefreshSerializer):
